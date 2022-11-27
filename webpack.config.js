f4197bc407c9106d3e17c42f65aaa35d441b9ba2
adebowale <ademibowaleadegboye@gmail.com>
2022-11-27 06:55:16 +0100

    modified webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
         test: /\.svg$/,
         use: 'svg-inline-loader',
       },
